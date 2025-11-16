29b689ffde016c90d1d073f591d2ec72e9ccba31
gautambudhiraja <37044923+gautambudhiraja@users.noreply.github.com>
2025-11-16 17:44:27 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -2,7 +2,7 @@
 
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, Gautam budhiraja!"); 
+        System.out.println("Hello, Gautam budhiraja! hershit budhiraja"); 
         System.out.println("Hello, Gautam budhiraja!"); 
     }
 }

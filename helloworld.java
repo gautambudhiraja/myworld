efaec504101a8c1c557bfbde9f6adfcd2a1184b1
gautambudhiraja <37044923+gautambudhiraja@users.noreply.github.com>
2025-11-16 17:43:13 +0530

    Duplicate greeting message in HelloWorld class

--- helloworld.java
+++ helloworld.java
@@ -3,5 +3,6 @@
 class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, Gautam budhiraja!"); 
+        System.out.println("Hello, Gautam budhiraja!"); 
     }
 }
